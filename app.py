2c9659bb496d07a338aa8ea30435f8c4a000b0a3
Jay Dhamale <68109991+jaydhamale7@users.noreply.github.com>
2021-01-20 09:51:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,45 @@
-import re
-import numpy as np
 import pandas as pd
 import nltk
+import numpy as np
 from bs4 import BeautifulSoup
 import unicodedata
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-stop_words = set(stopwords.words('english'))  
+import re
 import streamlit as st
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import one_hot
+stop_words = set(stopwords.words('english')) 
+import pickle
+pickle_in = open("tokenizer.pickle","rb")
+tokenizer = pickle.load(pickle_in)
 
-model = load_model('model.h5')
-ps = PorterStemmer()
+model =  load_model('sent.h5')
+
 def preprocess(x):
+    ps = PorterStemmer()
     x = str(x).lower()
     x = re.sub(r'([a-z0-9+._-]+@[a-z0-9+._-]+\.[a-z0-9+_-]+)',"", x) # remove email
-    x = re.sub(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', '' , x) #urls
+    x = re.sub(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', '' , x) # remove urls
     x = BeautifulSoup(x, 'lxml').get_text().strip() # remove html tag
-    x = re.sub(r'\brt\b', '', x).strip()
-    x = unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-    x = ' '.join([t for t in x.split() if t not in stop_words])
-    x = re.sub(r'[^\w ]+', "", x)
-    x = ' '.join(ps.stem(i) for i in x.split())
+    x = re.sub(r'\brt\b', '', x).strip() # remove retweets
+    x = unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8', 'ignore') # Remove Accented Characters
+    x = re.sub(r'[^\w ]+', "", x) # Special Characters 
+    x = ' '.join([t for t in x.split() if t not in stop_words]) # Remove Stop words
+    x = ' '.join(ps.stem(i) for i in x.split()) # Stemming
     return x
 
 st.write('# Sentiment Analysis App')
-user_input = st.text_area("Enter Text")
-
-if st.button("Predict"):
-    x = preprocess(user_input)
-    x = [one_hot(words,3000) for words in [x]]
-    x = pad_sequences(x,maxlen=20,)
-    pred = np.argmax(model.predict(x))
-    if pred == 0:
-        st.write(f"## Positive")
+txt = st.text_input("Enter text")
+if st.button('Predict'):
+ 
+    cleaned_text = preprocess(txt)
+    tokenized_text = tokenizer.texts_to_sequences([cleaned_text])
+    padded = pad_sequences(tokenized_text,maxlen=20)
+    pred = model.predict(padded)[0]
+#     st.write(pred)
+    if pred[1] >= 0.5:
+        st.write("### Negative")
     else:
-        st.write(f"## Negative")
+        st.write("### Positive")
